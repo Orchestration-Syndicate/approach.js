bad6607f7facd7ffe592f1b2c5530765bfe70c64
newtoallofthis123 <noobscience123@gmail.com>
2024-10-19 23:51:00 +0530

    [fix] Change to js for testing

---
+++ index.js
@@ -0,0 +1,17 @@
+import { XML } from "./approach/XML/Xml";
+import { HTML } from "./approach/HTML/Html";
+
+let xml = new HTML("html", "", [], {"lang": "en"});
+let head = new HTML("head");
+let body = new HTML("body", "", ["body"]);
+let title = new HTML("title", "", [], {}, "Hello, World!");
+let h1 = new HTML("h1");
+h1.content = "Hello, World!";
+
+xml[0] = head;
+xml[1] = body;
+head[0] = title;
+body[0] = h1;
+
+let result = xml.render();
+console.log(result);
