4b43d98c9f202c2df0837f4e67bf09bae0c5e42d
mrohith29 <mariyalarohith29@gmail.com>
2024-10-23 22:10:29 +0530

    extending HTML class to test

--- tests/render.test.js
+++ tests/render.test.js
@@ -1,6 +1,7 @@
 import { describe, expect, test } from 'bun:test'
 import { Stream } from '../approach/Render/Stream/Stream';
 import { Node } from '../approach/Render/Node/Node';
+import { HTML } from '../approach/Render/HTML/Html';
 
 describe("Stream", () => {
     test("should all the needed functions", () => {
@@ -28,5 +29,40 @@ describe("Node", () => {
 
         expect(node.render()).toBe("Hello World");
         expect(node._node_labels).toEqual(["world"]);
+    })
+})
+
+describe("Html", () => {
+    test("Html class Tests", () => {
+        let tag = 'div';
+        let content = 'Hello World';
+        let attributes = {};
+        let classes = ['class1', 'class2'];
+        let selfContained = false;
+        let id = 'test-id';
+        let styles = {color: 'red', 'font-size': '16px'};
+
+        let htmlElement = new HTML(tag, id, classes, attributes, content, styles, selfContained);
+        
+        expect(htmlElement.tag).toBe(tag);
+        expect(htmlElement.content).toBe(content);
+        expect(htmlElement.attributes['class']).toBe('class1 class2');
+        expect(htmlElement.attributes['id']).toBe(id);
+        expect(htmlElement.attributes['style']).toBe('color: red; font-size: 16px');
+    });
+
+    test('should render the Html elements correctly', () => {
+        let tag = 'div';
+        let content = 'Hello World';
+        let attributes = {};
+        let classes = ['class1', 'class2'];
+        let selfContained = false;
+        let id = 'test-id';
+        let styles = {color: 'red', 'font-size': '16px'};
+
+        let htmlElement = new HTML(tag, id, classes, attributes, content, styles, selfContained);
+        let expectedOutput = "<div class=\"class1 class2\" id=\"test-id\" style=\"color: red; font-size: 16px\">Hello World</div>\n";
+
+        expect(htmlElement.render()).toBe(expectedOutput);
     })
 })
