a7e8ae8a9aedacfc45ff4feea9a3097bb8a2b86c
newtoallofthis123 <noobscience123@gmail.com>
2024-10-25 23:14:14 +0530

    Migrate to xmldoc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+import { Attribute } from "./approach/Render/HTML/Attribute";
 import { HTML } from "./approach/Render/HTML/Html";
 
-let xml = new HTML("html", "", [], {"lang": "en"});
+let xml = new HTML("html", "", [], { lang: "en" });
 let head = new HTML("head");
 let body = new HTML("body", "", ["body"]);
 let title = new HTML("title", "", [], {}, "Hello, World!");
@@ -12,5 +13,8 @@ xml[1] = body;
 head[0] = title;
 body[0] = h1;
 
+let x = new Attribute("class", "container");
+
 let result = xml.render();
 console.log(result);
+console.log(x.render());
