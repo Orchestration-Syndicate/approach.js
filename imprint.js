67dc684b259251449385f825b946a8befb46bf99
Ishan Joshi <78465651+newtoallofthis123@users.noreply.github.com>
2024-11-04 23:17:41 +0530

    Discard changes to imprint.js

--- imprint.js
+++ imprint.js
@@ -1,15 +1,6 @@
 import { Imprint } from './approach/Imprint/Imprint';
-import { Container } from './approach/Render/Container/Container';
-import { HTML } from './approach/Render/HTML/Html';
 
-let x = new HTML();
-let y = new Container();
 let imp = new Imprint('./support/patterns/hello.xml', './support', '..');
-// imp.Prepare();
+imp.Prepare();
 
-// imp.Mint('cool');
-
-let instance = new globalThis['HTML']();
-let filteredArgs = imp.filterConstructorParams(instance, [{tag: 'div', content: 'Hello joshi', id: 'id2'}])
-console.log(imp.getConstructorParams(instance.constructor.toString()))
-console.log(filteredArgs);
+imp.Mint('cool');
